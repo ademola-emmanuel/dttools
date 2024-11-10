2f469f5394b9cabcd4fbea6d33a2f1928294c378
Emmanuel <ademolaemmanuel2@yahoo.com>
2024-11-10 22:15:30 +0000

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dttools",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple library for common date and time manipulations",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
